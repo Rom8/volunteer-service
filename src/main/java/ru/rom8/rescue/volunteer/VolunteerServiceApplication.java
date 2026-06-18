c3467b0770d79157d33d87cf7f3abb8831d8ccd7
Rom8 <roman.garkavenko@ya.ru>
2026-06-19 00:08:41 +0300

    initial commit

---
+++ src/main/java/ru/rom8/rescue/volunteer/VolunteerServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.rom8.rescue.volunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(VolunteerServiceApplication.class, args);
+    }
+
+}
