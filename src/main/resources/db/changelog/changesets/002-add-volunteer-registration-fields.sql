49b573023506a6e261c81cad529842e71f84fdc6
Rom8 <roman.garkavenko@ya.ru>
2026-07-02 21:54:09 +0300

    убрать значение по умолчанию

--- src/main/resources/db/changelog/changesets/002-add-volunteer-registration-fields.sql
+++ src/main/resources/db/changelog/changesets/002-add-volunteer-registration-fields.sql
@@ -2,7 +2,7 @@
 
 --changeset rom8:004-add-volunteer-registration-fields
 ALTER TABLE volunteer
-    ADD COLUMN gender VARCHAR(10) NOT NULL DEFAULT 'MALE'
+    ADD COLUMN gender VARCHAR(10) NOT NULL
         CHECK (gender IN ('MALE', 'FEMALE')),
     ADD COLUMN birth_date DATE NOT NULL;
 --rollback ALTER TABLE volunteer DROP COLUMN birth_date, DROP COLUMN gender;
