9eb62a5b35914cfe7a70dcd1199f3d984abca015
rom8 <roman.garkavenko@ya.ru>
2026-07-24 03:27:19 +0300

    Добавить маппинг названий населённых пунктов

--- src/main/java/ru/rom8/rescue/volunteer/mapper/VolunteerMapper.java
+++ src/main/java/ru/rom8/rescue/volunteer/mapper/VolunteerMapper.java
@@ -1,9 +1,12 @@
 package ru.rom8.rescue.volunteer.mapper;
 
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import ru.rom8.rescue.volunteer.api.model.VolunteerDto;
 import ru.rom8.rescue.volunteer.api.model.VolunteerRegisterRequest;
+import ru.rom8.rescue.volunteer.domain.entity.Location;
 import ru.rom8.rescue.volunteer.domain.entity.Volunteer;
 
 import java.util.List;
@@ -11,8 +14,21 @@ import java.util.List;
 @Mapper(componentModel = "spring", uses = ContactInfoMapper.class)
 public interface VolunteerMapper {
 
+    @Mapping(target = "settlementName", ignore = true)
+    @Mapping(target = "settlementDistrictName", ignore = true)
     @Mapping(target = "locationId", source = "location.id")
     VolunteerDto toDto(Volunteer volunteer);
+
+    @AfterMapping
+    default void mapSettlementNames(Volunteer volunteer, @MappingTarget VolunteerDto volunteerDto) {
+        Location location = volunteer.getLocation();
+        if (location.getParent() == null) {
+            volunteerDto.setSettlementName(location.getName());
+        } else {
+            volunteerDto.setSettlementDistrictName(location.getName());
+            volunteerDto.setSettlementName(location.getParent().getName());
+        }
+    }
 
     List<VolunteerDto> toDtoList(List<Volunteer> volunteers);
 
